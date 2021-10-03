37508b7c1c45d107a644b614d6ba854f589ab999
khanhdoan09 <80077540+khanhdoan09@users.noreply.github.com>
2021-10-03 09:40:07 +0700

    update product.html
    update phần lọc tìm kiếm

--- project/html.lionode.com/timewatch/Watch Time/javascript/khanh-js.js
+++ project/html.lionode.com/timewatch/Watch Time/javascript/khanh-js.js
@@ -1,3 +1,4 @@
+// search at top
 function searchPopular() {
     $(".input-text-search-top").click(()=>{
         $(".contain-search-popular").css("display", "flex");
@@ -6,5 +7,23 @@ function searchPopular() {
         $(".contain-search-popular").css("display", "none");
     });
 }
+$(window).ready(searchPopular, dropdownLocate);
 
-$(window).ready(searchPopular);
+// dropdown
+function dropdownLocate() {
+    $(".open-dropdown-locate").click(()=>{
+        $(".dropdown-locate").css("display", "inline");
+        $(".open-dropdown-locate").css("display", "none");
+        $(".close-dropdown-locate").css("display", "inline");
+        $(".contain-dropdown-locate").css("height", "550px");
+    });
+
+    $(".close-dropdown-locate").click(()=>{
+        $(".dropdown-locate").css("display", "none");
+        $(".open-dropdown-locate").css("display", "inline");
+        $(".close-dropdown-locate").css("display", "none");
+        $(".contain-dropdown-locate").css("height", "330px");
+    });
+}
+
+$(window).ready(dropdownLocate);
