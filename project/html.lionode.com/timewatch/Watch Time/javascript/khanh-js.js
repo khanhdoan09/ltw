cf3204d595e68d8a8f0d17fc6d299d702b234f36
khanhdoan09 <80077540+khanhdoan09@users.noreply.github.com>
2021-10-05 14:08:41 +0700

    update category.html, product.html
    update phần menu tìm kiếm, checkout, lọc tìm kiếm

--- project/html.lionode.com/timewatch/Watch Time/javascript/khanh-js.js
+++ project/html.lionode.com/timewatch/Watch Time/javascript/khanh-js.js
@@ -3,10 +3,11 @@ function searchPopular() {
     $(".input-text-search-top").click(()=>{
         $(".contain-search-popular").css("display", "flex");
     });
-    $(".contain-search-popular").mouseleave(()=>{
+    $(".contain-search").mouseleave(()=>{
         $(".contain-search-popular").css("display", "none");
     });
 }
+
 $(window).ready(searchPopular, dropdownLocate);
 
 // dropdown
@@ -27,3 +28,41 @@ function dropdownLocate() {
 }
 
 $(window).ready(dropdownLocate);
+
+
+
+function changeCategoryNav() {
+    $(".category-football").mouseover(()=>{
+        setUpCategoryNav();
+        $("#item-category-football").css("display", "flex");
+    });
+    $(".category-gym").mouseover(()=>{
+        setUpCategoryNav();
+        $("#item-category-gym").css("display", "flex");
+    });
+    $(".category-running").mouseover(()=>{
+        setUpCategoryNav();
+        $("#item-category-running").css("display", "flex");
+    });
+    $(".category-brand").mouseover(()=>{
+        setUpCategoryNav();
+        $("#item-category-brand").css("display", "flex");
+    });
+
+    $(".nav-detail-info").mouseover(()=>{
+        $(".nav-detail-info").css("display", "grid");
+    });
+
+    $(".navbar-collapse").mouseout(()=>{
+        $(".nav-detail-info").css("display", "none");
+    });
+}
+function setUpCategoryNav() {
+    $(".nav-detail-info").css("display", "grid");
+    $("#item-category-gym").css("display", "none");
+    $("#item-category-football").css("display", "none")
+    $("#item-category-running").css("display", "none");
+    $("#item-category-brand").css("display", "none");
+}
+
+$(window).ready(changeCategoryNav);
