776111363da450e3015b6867568469b8e94107a4
khanhdoan09 <80077540+khanhdoan09@users.noreply.github.com>
2021-10-19 10:29:13 +0700

    update responsive

--- project/html.lionode.com/timewatch/Watch Time/javascript/khanh-js.js
+++ project/html.lionode.com/timewatch/Watch Time/javascript/khanh-js.js
@@ -1,9 +1,9 @@
 // search at top
 function searchPopular() {
-    $(".input-text-search-top").click(()=>{
+    $(".input-text-search-top").click(() => {
         $(".contain-search-popular").css("display", "flex");
     });
-    $(".contain-search").mouseleave(()=>{
+    $(".contain-search").mouseleave(() => {
         $(".contain-search-popular").css("display", "none");
     });
 }
@@ -12,21 +12,23 @@ $(window).ready(searchPopular, dropdownLocate);
 
 // filter search {
 function toggleFilterSearch() {
-    $(".toggle-filter-search").click(()=>{
-        $(".contain-filter-search").slideToggle('slow');
+    $(".toggle-filter-search").click(() => {
+        $(".contain-filter-search").slideToggle("show");
     });
 }
+
 $(window).ready(toggleFilterSearch);
+
 // dropdown
 function dropdownLocate() {
-    $(".open-dropdown-locate").click(()=>{
+    $(".open-dropdown-locate").click(() => {
         $(".dropdown-locate").css("display", "inline");
         $(".open-dropdown-locate").css("display", "none");
         $(".close-dropdown-locate").css("display", "inline");
         $(".contain-dropdown-locate").css("height", "550px");
     });
 
-    $(".close-dropdown-locate").click(()=>{
+    $(".close-dropdown-locate").click(() => {
         $(".dropdown-locate").css("display", "none");
         $(".open-dropdown-locate").css("display", "inline");
         $(".close-dropdown-locate").css("display", "none");
@@ -36,34 +38,57 @@ function dropdownLocate() {
 
 $(window).ready(dropdownLocate);
 
+// click price search
+function clickPrice() {
+    $("#price-search-01").click(() => {
+        $("#price-search-01").css("background-color", "#5f2525");
+    });
+    $("#price-search-02").click(() => {
+        $("#price-search-02").css("background-color", "#5f2525");
+    });
+    $("#price-search-03").click(() => {
+        $("#price-search-03").css("background-color", "#5f2525");
+    });
+}
+
+$(window).ready(clickPrice);
 
 // navigation
 function changeCategoryNav() {
-    $(".category-football").mouseover(()=>{
+    $("#category-football").mouseover(() => {
         setUpCategoryNav();
         $("#item-category-football").css("display", "flex");
+        if ($(window).width() < 750)
+            $(".nav-detail-info").css("top", "80px");
     });
-    $(".category-gym").mouseover(()=>{
+    $("#category-gym").mouseover(() => {
         setUpCategoryNav();
         $("#item-category-gym").css("display", "flex");
+        if ($(window).width() < 750)
+            $(".nav-detail-info").css("top", "120px");
     });
-    $(".category-running").mouseover(()=>{
+    $("#category-running").mouseover(() => {
         setUpCategoryNav();
         $("#item-category-running").css("display", "flex");
+        if ($(window).width() < 750)
+            $(".nav-detail-info").css("top", "160px");
     });
-    $(".category-brand").mouseover(()=>{
+    $("#category-brand").mouseover(() => {
         setUpCategoryNav();
         $("#item-category-brand").css("display", "flex");
+        if ($(window).width() < 750)
+            $(".nav-detail-info").css("top", "200px");
     });
 
-    $(".nav-detail-info").mouseover(()=>{
+    $(".nav-detail-info").mouseover(() => {
         $(".nav-detail-info").css("display", "grid");
     });
 
-    $(".navbar-collapse").mouseout(()=>{
+    $(".navbar-collapse").mouseout(() => {
         $(".nav-detail-info").css("display", "none");
     });
 }
+
 function setUpCategoryNav() {
     $(".nav-detail-info").css("display", "grid");
     $("#item-category-gym").css("display", "none");
@@ -149,9 +174,9 @@ function displayCategory() {
             "            <div class=\"image product-imageblock\">\n" +
             "              <a href=\"product.html\">" +
             "                  <div class=\"related-product-sale\">\n" +
-            "                     <h5 style='margin-top: 10px'>" +value.saleRate + "</h5>\n" +
+            "                     <h5 style='margin-top: 10px'>" + value.saleRate + "</h5>\n" +
             "                   </div>" +
-            " <img src=\""+value.linkSrc+"\" alt=\"lorem ippsum dolor dummy\" title=\"lorem ippsum dolor dummy\" class=\"img-responsive\" /> </a>\n" +
+            " <img src=\"" + value.linkSrc + "\" alt=\"lorem ippsum dolor dummy\" title=\"lorem ippsum dolor dummy\" class=\"img-responsive\" /> </a>\n" +
             "              <div class=\"button-group\">\n" +
             "                <button type=\"button\" class=\"wishlist\" data-toggle=\"tooltip\" title=\"Thêm vào yêu thích\"><i class=\"fas fa-heart\"></i></button>\n" +
             "                <button type=\"button\" class=\"addtocart-btn\">Mua ngay</button>\n" +
@@ -159,13 +184,13 @@ function displayCategory() {
             "              </div>\n" +
             "            </div>\n" +
             "            <div class=\"caption product-detail\">\n" +
-            "           <h2 style='margin-top: 5px; text-transform: uppercase'>"+value.brand+"</h2>"+
+            "           <h2 style='margin-top: 5px; text-transform: uppercase'>" + value.brand + "</h2>" +
             "              <h4 class=\"product-name\"> " +
             "<a href=\"product.html\" title=\"lorem ippsum dolor dummy\" style='text-transform: capitalize'>"
-            + value.nameProduct  + " </a> </h4>\n" +
+            + value.nameProduct + " </a> </h4>\n" +
             "              <p class=\"product-desc\"> More room to move. With 80GB or 160GB of storage and up to 40 hours of battery life, the new lorem ippsum dolor dummy lets you enjoy up to 40,000 songs or up to 200 hours of video or any combination wherever you go. Cover\n" +
             "                Flow. Browse through your music collection by flipping..</p>\n" +
-            "              <p class=\"price product-price\"><span class=\"price-old\" style='margin-right: 10px'>"+value.oldPrice+"</span>"+value.price+" <span class=\"price-tax\">Ex Tax: $100.00</span> </p>\n" +
+            "              <p class=\"price product-price\"><span class=\"price-old\" style='margin-right: 10px'>" + value.oldPrice + "</span>" + value.price + " <span class=\"price-tax\">Ex Tax: $100.00</span> </p>\n" +
             "              <div class=\"rating\"> <span class=\"fa fa-stack\"><i class=\"fa fa-star fa-stack-2x\"></i><i class=\"fa fa-star-o fa-stack-2x\"></i></span> <span class=\"fa fa-stack\"><i class=\"fa fa-star fa-stack-2x\"></i><i class=\"fa fa-star-o fa-stack-2x\"></i></span>                                    <span class=\"fa fa-stack\"><i class=\"fa fa-star fa-stack-2x\"></i><i class=\"fa fa-star-o fa-stack-2x\"></i></span> <span class=\"fa fa-stack\"><i class=\"fa fa-star-o fa-stack-2x\"></i></span> <span class=\"fa fa-stack\"><i class=\"fa fa-star-o fa-stack-2x\"></i></span>                                    </div>\n" +
             "            </div>\n" +
             "            <div class=\"button-group\">\n" +
@@ -179,15 +204,16 @@ function displayCategory() {
         $(".grid-list-wrapper").append(contain);
     }));
 }
+
 $(window).ready(displayCategory);
 
 function displaySizeInRefineSearch() {
-    let sizeArr = [34, 35 ,36 ,37 ,38 ,39, 40, 41, 42, 43, 44, 45]
-    sizeArr.forEach((value)=> {
+    let sizeArr = [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
+    sizeArr.forEach((value) => {
         $(".contain-dropdown-size").append(
             " <label class=\"checkbox\">\n" +
             " <input name=\"filter[]\" type=\"checkbox\" value=\"1\" />\n" +
-            value+"</label>"
+            value + "</label>"
         );
     });
 }
