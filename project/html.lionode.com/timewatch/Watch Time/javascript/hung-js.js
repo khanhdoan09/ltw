0fede086d56c99bbfb1053d4b5bb0ef9e2e23f76
hungphan0501 <91454052+hungphan0501@users.noreply.github.com>
2021-10-19 21:20:23 +0700

    update login

--- project/html.lionode.com/timewatch/Watch Time/javascript/hung-js.js
+++ project/html.lionode.com/timewatch/Watch Time/javascript/hung-js.js
@@ -1,29 +1,23 @@
-const modal=document.querySelector('.js-modal');    
+const modal=document.querySelector('.js-modal');
 const modalLogin=document.querySelector('.js-login');
 const modalClose=document.querySelector('.js-modal-close');
 const modalContainer=document.querySelector('.js-modal-container');
 const modalLoginBtn=document.querySelector('.js-login-modal');
 
-
 // hàm hiển thị phần đăng nhập
 function showBuyLogin() {
     modal.classList.add('open')
 }
-
-
 // hàm ẩn modal đăng nhập
 function hideBuyLogin() {
     modal.classList.remove('open')
 }
-
-
-
 modalLogin.addEventListener('click' , showBuyLogin)
 
 modalClose.addEventListener('click', hideBuyLogin)
-// 
+//
 modal.addEventListener('click', hideBuyLogin)
-// 
+//
 modalContainer.addEventListener('click', function(even) {
     even.stopPropagation()
 })
@@ -32,9 +26,9 @@ const register = document.querySelector( '.js_register')
 const modalRegister=document.querySelector('.js-modal-register');
 const modalCloseRegister=document.querySelector('.js-modal-close-register');
 const modalContainerRegister=document.querySelector('.js-modal-container-register');
-const modalRegisterBtn=document.querySelector('.js-register')
+const modalRegisterBtn=document.querySelector('.js-register-modal')
 
-// hiện thị phần đăng ký 
+// hiện thị phần đăng ký
 function showModalRegister() {
     modalRegister.classList.add('open')
 }
@@ -45,13 +39,14 @@ function hideModalRegister() {
 }
 
 register.addEventListener('click', showModalRegister)
-modalRegisterBtn.addEventListener('click', showModalRegister)
-modalRegisterBtn.addEventListener('click', hideBuyLogin)
+// modalRegisterBtn.addEventListener('click', showModalRegister)
 modalCloseRegister.addEventListener('click', hideModalRegister)
 modalRegister.addEventListener('click', hideModalRegister)
 
+modalRegisterBtn.addEventListener('click', showModalRegister)
+modalRegisterBtn.addEventListener('click', hideBuyLogin)
 modalLoginBtn.addEventListener('click', showBuyLogin)
-modalLoginBtn.addEventListener('click' ,hideModalRegister)   
+modalLoginBtn.addEventListener('click' ,hideModalRegister)
 
 
 modalContainerRegister.addEventListener('click', function(even) {
