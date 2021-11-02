db2654c8eac0bfd488f75bb9df3452d068066875
khanhdoan09 <80077540+khanhdoan09@users.noreply.github.com>
2021-11-02 20:48:16 +0700

    test

--- project/html.lionode.com/timewatch/Watch Time/javascript/khanh-js.js
+++ project/html.lionode.com/timewatch/Watch Time/javascript/khanh-js.js
@@ -1,6 +1,5 @@
-
-
 // search at top
+
 function searchPopular() {
     $(".input-text-search-top").click(() => {
         $(".contain-search-popular").css("display", "flex");
@@ -111,74 +110,78 @@ function Product(brand, nameProduct, linkSrc, price, saleRate, oldPrice) {
     this.oldPrice = oldPrice;
 }
 
-function displayCategory() {
-    let arrProduct = [
-        new Product("under armour",
-            "tri base reign 2 trainers mens",
-            "image/product/product-01.jpg",
-            "87.99$",
-            "-10%",
-            "99.99$"),
-        new Product("under armour",
-            "tri base reign 3 NM",
-            "image/product/product-02.jpg",
-            "80.00$",
-            "-8%",
-            "90.59$"),
-        new Product("nike",
-            "Metcon 7 Ladies Training Shoes",
-            "image/product/product-03.webp",
-            "114.00$",
-            "-12%",
-            "120.99$"),
-        new Product("nike",
-            "Metcon 6 Mens Training Shoes",
-            "image/product/product-04.webp",
-            "86.00$",
-            "-20%",
-            "100.99$"),
-        new Product("nike",
-            "Metcon Flyknit Mens Training Shoes",
-            "image/product/product-05.jpg",
-            "95.00$",
-            "-22%",
-            "110.00$"),
-        new Product("adidas",
-            "Adios 6 Trainer",
-            "image/product/product-06.jpg",
-            "87.00$",
-            "-25%",
-            "100.00$"),
-        new Product("adidas",
-            "SL20.2 Running Shoes Mens",
-            "image/product/product-07.webp",
-            "79.99$",
-            "-19%",
-            "99.99$"),
-        new Product("puma",
-            "SL20.2 Running Shoes Mens",
-            "image/product/product-08.jpg",
-            "40.99$",
-            "-9%",
-            "50.99$"),
-        new Product("nike",
-            "Metcon Flyknit Mens Training Shoes",
-            "image/product/product-09.webp",
-            "92.99$",
-            "-3%",
-            "95.99$")
-    ];
+// let arrProduct = [
+//     new Product("under armour",
+//         "tri base reign 2 trainers mens",
+//         "image/product/product-01.jpg",
+//         "87.99$",
+//         "-10%",
+//         "99.99$"),
+//     new Product("under armour",
+//         "tri base reign 3 NM",
+//         "image/product/product-02.jpg",
+//         "80.00$",
+//         "-8%",
+//         "90.59$"),
+//     new Product("nike",
+//         "Metcon 7 Ladies Training Shoes",
+//         "image/product/product-03.webp",
+//         "114.00$",
+//         "-12%",
+//         "120.99$"),
+//     new Product("nike",
+//         "Metcon 6 Mens Training Shoes",
+//         "image/product/product-04.webp",
+//         "86.00$",
+//         "-20%",
+//         "100.99$"),
+//     new Product("nike",
+//         "Metcon Flyknit Mens Training Shoes",
+//         "image/product/product-05.jpg",
+//         "95.00$",
+//         "-22%",
+//         "110.00$"),
+//     new Product("adidas",
+//         "Adios 6 Trainer",
+//         "image/product/product-06.jpg",
+//         "87.00$",
+//         "-25%",
+//         "100.00$"),
+//     new Product("adidas",
+//         "SL20.2 Running Shoes Mens",
+//         "image/product/product-07.webp",
+//         "79.99$",
+//         "-19%",
+//         "99.99$"),
+//     new Product("puma",
+//         "SL20.2 Running Shoes Mens",
+//         "image/product/product-08.jpg",
+//         "40.99$",
+//         "-9%",
+//         "50.99$"),
+//     new Product("nike",
+//         "Metcon Flyknit Mens Training Shoes",
+//         "image/product/product-09.webp",
+//         "92.99$",
+//         "-3%",
+//         "95.99$")
+// ];
 
-
-    arrProduct.forEach(((value) => {
+function displayCategory(position = 0) {
+    $(".grid-list-wrapper").text(""); // reset after choose pagination
+    let arrProduct = NaturalGrass;
+    for (let i = position * 9; i < (position * 9) + 9; i++) {
+        if (i == arrProduct.length - 1)
+            break;
+        let value = arrProduct[i];
         let contain = "<div class=\"product-layout product-grid col-lg-4 col-md-5 col-sm-6 col-xs-10\">\n" +
             "          <div class=\"product-thumb\">\n" +
             "            <div class=\"image product-imageblock\">\n" +
             "              <a href=\"product.html\">" +
             "                  <div class=\"related-product-sale\">\n" +
-            "                     <h5 style='margin-top: 10px'>" + value.saleRate + "</h5>\n" +
+            "                     <h5 style='margin-top: 10px'>" + 10 % +"</h5>\n" +
             "                   </div>" +
-            " <img src=\"" + value.linkSrc + "\" alt=\"lorem ippsum dolor dummy\" title=\"lorem ippsum dolor dummy\" class=\"img-responsive\" /> </a>\n" +
+            " <img src=\"" + value.img1 + "\" alt=\"lorem ippsum dolor dummy\" title=\"lorem ippsum dolor dummy\" class=\"img-responsive\" /> </a>\n" +
             "              <div class=\"button-group\">\n" +
             "                <button type=\"button\" class=\"wishlist\" data-toggle=\"tooltip\" title=\"Thêm vào yêu thích\"><i class=\"fas fa-heart\"></i></button>\n" +
             "                <button type=\"button\" class=\"addtocart-btn\">Mua ngay</button>\n" +
@@ -188,11 +191,11 @@ function displayCategory() {
             "            <div class=\"caption product-detail\">\n" +
             "           <h2 style='margin-top: 5px; text-transform: uppercase'>" + value.brand + "</h2>" +
             "              <h4 class=\"product-name\"> " +
-            "<a href=\"product.html\" title=\"lorem ippsum dolor dummy\" style='text-transform: capitalize'>"
-            + value.nameProduct + " </a> </h4>\n" +
+            "<a href=\"product.html\" title=\"lorem ippsum dolor dummy\" style='text-transform: capitalize'>" +
+            value.name + " </a> </h4>\n" +
             "              <p class=\"product-desc\"> More room to move. With 80GB or 160GB of storage and up to 40 hours of battery life, the new lorem ippsum dolor dummy lets you enjoy up to 40,000 songs or up to 200 hours of video or any combination wherever you go. Cover\n" +
             "                Flow. Browse through your music collection by flipping..</p>\n" +
-            "              <p class=\"price product-price\"><span class=\"price-old\" style='margin-right: 10px'>" + value.oldPrice + "</span>" + value.price + " <span class=\"price-tax\">Ex Tax: $100.00</span> </p>\n" +
+            "              <p class=\"price product-price\"><span class=\"price-old\" style='margin-right: 10px'>" + value.price + 0 + "</span>" + value.price + " <span class=\"price-tax\">Ex Tax: $100.00</span> </p>\n" +
             "              <div class=\"rating\"> <span class=\"fa fa-stack\"><i class=\"fa fa-star fa-stack-2x\"></i><i class=\"fa fa-star-o fa-stack-2x\"></i></span> <span class=\"fa fa-stack\"><i class=\"fa fa-star fa-stack-2x\"></i><i class=\"fa fa-star-o fa-stack-2x\"></i></span>                                    <span class=\"fa fa-stack\"><i class=\"fa fa-star fa-stack-2x\"></i><i class=\"fa fa-star-o fa-stack-2x\"></i></span> <span class=\"fa fa-stack\"><i class=\"fa fa-star-o fa-stack-2x\"></i></span> <span class=\"fa fa-stack\"><i class=\"fa fa-star-o fa-stack-2x\"></i></span>                                    </div>\n" +
             "            </div>\n" +
             "            <div class=\"button-group\">\n" +
@@ -204,10 +207,34 @@ function displayCategory() {
             "        </div>";
 
         $(".grid-list-wrapper").append(contain);
-    }));
+    }
+
 }
 
-$(window).ready(displayCategory);
+function displayPagination() {
+    let num = Math.ceil(NaturalGrass.length / 9);
+    let contain = "";
+    for (let i = 1; i <= num; i++) {
+        contain += `<li><a id="page-${i}">${i}</a></li>`;
+    }
+    $(".pagination").append(contain);
+}
+
+$(window).ready(displayPagination);
+
+function getPositionPagination() {
+    displayCategory(0); // init
+    let arrPg = $(".pagination li a");
+    let pos = 1;
+    for (let i = 0; i < arrPg.length; i++) {
+        arrPg[i].addEventListener("click", () => {
+            pos = arrPg[i].text;
+            displayCategory(+pos - 1);
+        });
+    }
+}
+
+$(window).ready(getPositionPagination);
 
 function displaySizeInRefineSearch() {
     let sizeArr = [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
@@ -221,6 +248,7 @@ function displaySizeInRefineSearch() {
 }
 
 $(window).ready(displaySizeInRefineSearch);
+
 
 // access information of page costumer
 function accessInfoCustomer() {
@@ -289,15 +317,15 @@ $(window).ready(accessInfoCustomer);
 // load location customer
 function loadLocation() {
     loadCity();
-    loadDistrict(89,);
+    loadDistrict(89, );
     loadWard(883);
 }
 
 function chooseLocation() {
     loadLocation();
-    $('#contain-option-city').on('change', function () {
+    $('#contain-option-city').on('change', function() {
         let cityCode = $(this).val();
-        loadDistrict(cityCode,);
+        loadDistrict(cityCode, );
         let codeFirstDistrict = "";
         for (let i = 0; i < districts.length; i++) {
             if (districts[i].parent_code == cityCode) {
@@ -308,7 +336,7 @@ function chooseLocation() {
         loadWard(codeFirstDistrict);
     });
 
-    $('#contain-option-district').on('change', function (e) {
+    $('#contain-option-district').on('change', function(e) {
         let districtCode = $(this).val();
         loadWard(districtCode);
     });
@@ -344,5 +372,5 @@ function loadWard(wardCode) {
 }
 
 $(window).ready(chooseLocation);
-
-
+$(window).ready(chooseLocation);
+$(window).ready(chooseLocation);
